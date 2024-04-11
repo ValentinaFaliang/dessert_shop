266c4bf6fb83ca3358e7dc6d8f668c1b3a0167d7
Faliang <rukatata@gmail.com>
2024-04-11 17:48:00 +0300

    add configurators

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+// const EslintWebpackPlugin = require('eslint-webpack-plugin');
+// const StylelintWebpackPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'production',
+  entry: './src/script.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.{js|jsx}$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+    // new EslintWebpackPlugin({
+    //   files: '{**/*,*}.{tsx,ts,js}'
+    // }),
+    // new StylelintWebpackPlugin({
+    //   files: '{**/*,*}.css'
+    // })
+  ],
+  devServer: {
+    open: true
+  }
+};
