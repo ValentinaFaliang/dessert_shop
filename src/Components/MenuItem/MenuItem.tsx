638acd1d935f406623eeae5935c45dbfa0c4b935
Faliang <rukatata@gmail.com>
2024-09-19 12:33:18 +0300

    add new design of items menu

--- src/Components/MenuItem/MenuItem.tsx
+++ src/Components/MenuItem/MenuItem.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import zefir1 from './../../assets/menu-items/zefirka.png';
 import './MenuItem.css';
 
 export const MenuItem = () => {
   return (
     <section className="menu-item">
       <figure className="menu-item__container">
-        <img className="menu-item__image" src={zefir1} alt="soda_can" />
+        <div className="menu-item__image"></div>
         <figcaption className="menu-item__description">
           <div className="menu-item__title">
             <h3>Original</h3>
